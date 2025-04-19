b921dadaedff441a6e73ec7b656373ceb3ed3fd0
rozgursimsek <rozgursimsek@gmail.com>
2025-04-19 15:47:08 +0300

    [dev] added Semaphore Example

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,94 @@
+// Queue Example
+// #include <Arduino.h>
+
+// QueueHandle_t myQueue;  // Queue handle
+
+// void TaskBlink(void *pvParameters) {
+//   pinMode(2, OUTPUT);  //LED (GPIO 2)
+//   while (1) {
+//     digitalWrite(2, HIGH);
+//     vTaskDelay(500 / portTICK_PERIOD_MS);
+//     digitalWrite(2, LOW);
+//     vTaskDelay(500 / portTICK_PERIOD_MS);
+    
+//     bool ledStatus = digitalRead(2);
+//     xQueueSend(myQueue, &ledStatus, 0);
+//   }
+// }
+
+// void TaskSerial(void *pvParameters) {
+//   bool ledStatus;
+//   while (1) {
+    
+//     if (xQueueReceive(myQueue, &ledStatus, portMAX_DELAY) == pdTRUE) {
+//       // LED state write serial monitor
+//       Serial.print("LED Durumu: ");
+//       Serial.println(ledStatus ? "ON" : "OFF");
+//     }
+//   }
+// }
+
+// void setup() {
+//   Serial.begin(9600);
+  
+//   myQueue = xQueueCreate(10, sizeof(bool));
+  
+//   // FreeRTOS duty 
+//   xTaskCreate(TaskBlink, "BlinkTask", 1000, NULL, 1, NULL);
+//   xTaskCreate(TaskSerial, "SerialTask", 1000, NULL, 1, NULL);
+// }
+
+// void loop() {
+//   // Main loop null, work in FreeRTOS function
+// }
+
+//Semaphore Example
 #include <Arduino.h>
 
-QueueHandle_t myQueue;  // Kuyruk handle
+SemaphoreHandle_t mySemaphore;  // Semaphore tanımı
 
-void TaskBlink(void *pvParameters) {
-  pinMode(2, OUTPUT);  // Dahili LED (GPIO 2)
+void TaskLED(void *pvParameters) {
+  pinMode(2, OUTPUT);
   while (1) {
-    digitalWrite(2, HIGH);
-    vTaskDelay(500 / portTICK_PERIOD_MS);
-    digitalWrite(2, LOW);
-    vTaskDelay(500 / portTICK_PERIOD_MS);
-    
-    // LED yanma durumunu kuyruğa gönder
-    bool ledStatus = digitalRead(2);
-    xQueueSend(myQueue, &ledStatus, 0);
+    if (xSemaphoreTake(mySemaphore, portMAX_DELAY) == pdTRUE) {
+      digitalWrite(2, HIGH);
+      Serial.println("LED ON");
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+
+      digitalWrite(2, LOW);
+      Serial.println("LED OFF");
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+
+      xSemaphoreGive(mySemaphore);  // Semaforu geri bırak
+    }
   }
 }
 
 void TaskSerial(void *pvParameters) {
-  bool ledStatus;
   while (1) {
-    // Kuyruktan LED durumunu al
-    if (xQueueReceive(myQueue, &ledStatus, portMAX_DELAY) == pdTRUE) {
-      // LED durumunu seri port üzerinden yazdır
-      Serial.print("LED Durumu: ");
-      Serial.println(ledStatus ? "ON" : "OFF");
+    if (xSemaphoreTake(mySemaphore, portMAX_DELAY) == pdTRUE) {
+      Serial.println("Görev 2: Seri portu kullaniyor...");
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      xSemaphoreGive(mySemaphore);  // Semaforu geri bırak
     }
   }
 }
 
 void setup() {
   Serial.begin(9600);
-  // Kuyruğu oluştur
-  myQueue = xQueueCreate(10, sizeof(bool));
-  
-  // FreeRTOS görevlerini oluştur
-  xTaskCreate(TaskBlink, "BlinkTask", 1000, NULL, 1, NULL);
-  xTaskCreate(TaskSerial, "SerialTask", 1000, NULL, 1, NULL);
+  delay(1000);  // Stabil başlatmak için kısa gecikme
+
+  // Mutex şeklinde semaphore oluşturuluyor
+  mySemaphore = xSemaphoreCreateMutex();
+
+  // Görevler oluşturuluyor
+  xTaskCreate(TaskLED, "LED Task", 1000, NULL, 1, NULL);
+  xTaskCreate(TaskSerial, "Serial Task", 1000, NULL, 1, NULL);
 }
 
 void loop() {
-  // Ana loop boş, çünkü işler FreeRTOS görevlerinde yapılacak
+  // FreeRTOS görevleri çalışıyor, loop boş
 }
+
 
 
